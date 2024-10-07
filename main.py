bbe957b8cdad8a1189e74b12536ee51f28010241
FinTechS24 <souad.hamida2@mail.dcu.ie>
2024-10-07 16:02:09 +0100

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.write("""
-Hello world , its my very  first streamlit 
+Hello world , its my second streamlit test
 """
          )
 df = pd.read_csv("mushrooms.csv")
