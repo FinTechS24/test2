eb1501a6a8fbe55801ab95367d9b689f116b7cda
FinTechS24 <souad.hamida2@mail.dcu.ie>
2024-10-07 15:47:19 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import numpy as np
+import streamlit as st
 import pandas as pd
-
-x = 6 + 7
-print(x)
-x
+import numpy as np
+st.write("""
+Hello world , its my very  first streamlit 
+"""
+         )
+df = pd.read_csv("mushrooms.csv")
+st.line_chart(df['season'])
